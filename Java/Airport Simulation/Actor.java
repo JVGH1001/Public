b5c92c2cb75635411d802c7cf7a47062639f5cc8
JVGH1001 <119895468+JVGH1001@users.noreply.github.com>
2024-12-14 16:03:03 -0500

    Add files via upload
    Airport Sim upload

---
+++ Java/Airport Simulation/Actor.java
@@ -0,0 +1,15 @@
+/**
+ * Actor Interface
+ * 
+ * @author Jeffrey VanMeter 
+ * Version 1 
+ * 02/20/2014
+ */
+
+/**
+ * Actor interface.
+ */
+public interface Actor
+{
+    void act (int tick);
+}
